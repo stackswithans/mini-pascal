15ac7aaaf1d9391adeaf362f3677930a7da562dd
stackswithans <stexor12@gmail.com>
2021-07-19 19:41:08 +0100

    Change project name

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import { readFileSync } from "fs";
 import * as process from "process";
 
 const main = () => {
-    console.log("HELOO");
     const filename = process.argv[2];
     const src = readFileSync(filename).toString("utf-8");
     const lexer = new Lexer(src);
-    console.log("HELOO 2");
     let token = lexer.nextToken();
     while (token.token !== TT.EOF) {
         console.log(`Type: ${token.token}`);
