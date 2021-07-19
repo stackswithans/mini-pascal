9573f2f91aa7ea88672017efa29a991c88f3352c
stackswithans <stexor12@gmail.com>
2021-07-19 17:50:29 +0100

    change tests

--- test.ts
+++ test.ts
@@ -32,42 +32,56 @@ const letters = [
 const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const delimeters = ["<", ">", ".", "!", "=", ":", ";", "/", "=", "+", "-"];
 
-equal(
-    letters.every((value) => isLetter(value)),
-    true
-);
+function testIsLetter() {
+    equal(
+        letters.every((value) => isLetter(value)),
+        true
+    );
 
-equal(
-    letters
-        .map((value) => value.toUpperCase())
-        .every((value) => isLetter(value)),
-    true
-);
+    equal(
+        letters
+            .map((value) => value.toUpperCase())
+            .every((value) => isLetter(value)),
+        true
+    );
 
-equal(
-    digits.every((value) => isLetter(value)),
-    false
-);
+    equal(
+        digits.every((value) => isLetter(value)),
+        false
+    );
 
-equal(
-    delimeters.every((value) => isLetter(value)),
-    false
-);
+    equal(
+        delimeters.every((value) => isLetter(value)),
+        false
+    );
+}
 
-equal(
-    digits.every((value) => isDigit(value)),
-    true
-);
+function testIsDigit() {
+    equal(
+        digits.every((value) => isDigit(value)),
+        true
+    );
 
-equal(
-    letters.every((value) => isDigit(value)),
-    false,
-    "Is digit failed"
-);
+    equal(
+        letters.every((value) => isDigit(value)),
+        false,
+        "Is digit failed"
+    );
 
-equal(
-    delimeters.every((value) => isDigit(value)),
-    false
-);
+    equal(
+        delimeters.every((value) => isDigit(value)),
+        false
+    );
+}
 
-console.log("All tests passed!");
+function main() {
+    console.log("Testing isDigit...");
+    testIsDigit();
+    console.log("isDigit has passed!\n\n");
+    console.log("Testing isLetter...");
+    testIsLetter();
+    console.log("isLetter has passed!\n\n");
+    console.log("All tests passed!");
+}
+
+main();
---
+++ test_files/teste2.pas
@@ -0,0 +1,8 @@
+var
+ A, B : integer;
+ C, D : real;
+begin
+ A := 1;
+ B := 3;
+end.
+'sssd'
