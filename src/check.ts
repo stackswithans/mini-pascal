41e4d4a87260f2e37ffa5477465b955e3133e1b7
stackswithans <stexor12@gmail.com>
2021-07-25 19:47:59 +0100

    Add parameter validation

--- src/check.ts
+++ src/check.ts
@@ -1,3 +1,4 @@
+import { Token } from "./token";
 import * as ast from "./ast";
 
 type SemError = { line: number; message: string; col: number };
@@ -17,6 +18,10 @@ class Checker {
     reportError<T extends ast.NodeData>(message: string, ctx: ast.Node<T>) {
         let errMsg = `Erro semântico (${ctx.line}:${ctx.col}): ${message}.`;
         this.errors.push({ message: errMsg, line: ctx.line, col: ctx.col });
+    }
+
+    declareSymbol(id: string, kind: ast.NodeKind, data: ast.NodeData) {
+        this.scope.symbols[id] = { kind, data };
     }
 
     checkBlock(block: ast.Block) {
@@ -44,17 +49,51 @@ class Checker {
                 this.checkVarDecl(varDecl);
                 break;
             }
+            case ast.NodeKind.SubRoutine: {
+                let funcDecl = node as ast.Node<ast.SubRoutine>;
+                this.checkfuncDecl(funcDecl);
+                break;
+            }
+            default:
+                console.log(`Not implement yet`);
         }
     }
 
     checkVarDecl(node: ast.Node<ast.VarDecl>) {
-        let { id, varType } = node.data;
+        let { id } = node.data;
         let idStr = id.lexeme;
         //Check if already
         if (idStr in this.scope.symbols) {
             this.reportError("Este identificador já foi declarado", node);
+            return;
         }
-        this.scope.symbols[idStr] = { kind: node.nodeKind, data: node.data };
+        this.declareSymbol(idStr, node.nodeKind, node.data);
+    }
+
+    checkfuncDecl(node: ast.Node<ast.SubRoutine>) {
+        let { name, formal_params } = node.data;
+        let nameStr = name.lexeme;
+        if (nameStr in this.scope.symbols) {
+            this.reportError("Este identificador já foi declarado", node);
+            return;
+        }
+        this.declareSymbol(nameStr, node.nodeKind, node.data);
+        let funcScope: Scope = { parent: this.scope, symbols: {} };
+        this.scope = funcScope;
+        //Verify args
+        //TODO: Dedup this code;
+        for (let decl of formal_params) {
+            let idStr = decl.data.id.lexeme;
+            if (idStr in this.scope.symbols) {
+                this.reportError(
+                    `O parâmetro '${idStr}' já foi declarado nesta função`,
+                    node
+                );
+                return;
+            }
+            this.declareSymbol(idStr, decl.nodeKind, node.data);
+        }
+        this.scope = funcScope.parent as Scope;
     }
 }
 
