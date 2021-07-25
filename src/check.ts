95c51434404582f400eba1438b6a15f8f9525289
stackswithans <stexor12@gmail.com>
2021-07-25 18:02:58 +0100

    Add variable declaration checking

--- src/check.ts
+++ src/check.ts
@@ -1,18 +1,68 @@
 import * as ast from "./ast";
 
-function typeCast<T extends ast.NodeData>(
-    node: ast.NodeData,
-    kind: ast.NodeKind
-): node is T {}
+type SemError = { line: number; message: string; col: number };
 
-export class Checker {
+type SymbolData = { kind: ast.NodeKind; data: ast.NodeData };
+type Symbol = Record<string, SymbolData>;
+
+interface Scope {
+    parent: Scope | null;
+    symbols: Symbol;
+}
+
+class Checker {
+    scope: Scope = { parent: null, symbols: {} };
+    errors: SemError[] = [];
+
+    reportError<T extends ast.NodeData>(message: string, ctx: ast.Node<T>) {
+        let errMsg = `Erro semântico (${ctx.line}:${ctx.col}): ${message}`;
+        console.log(errMsg);
+        this.errors.push({ message: errMsg, line: ctx.line, col: ctx.col });
+    }
+
+    checkBlock(block: ast.Block) {
+        const isFunction = (
+            node: ast.Node<ast.VarDecl | ast.SubRoutine>
+        ): node is ast.Node<ast.SubRoutine> => {
+            return node.nodeKind === ast.NodeKind.SubRoutine;
+        };
+        for (let decl of block.declarations) {
+            if (isFunction(decl)) {
+                this.check(decl);
+            } else {
+                this.check(decl);
+            }
+        }
+        for (let stmt of block.statements) {
+            this.check(stmt);
+        }
+    }
+
     check<T extends ast.NodeData>(node: ast.Node<T>) {
         switch (node.nodeKind) {
-            case ast.NodeKind.Program: {
-                this.checkBlock();
+            case ast.NodeKind.VarDecl: {
+                let varDecl = node as ast.Node<ast.VarDecl>;
+                this.checkVarDecl(varDecl);
+                break;
             }
         }
     }
 
-    private checkBlock(block: ast.Block) {}
+    checkVarDecl(node: ast.Node<ast.VarDecl>) {
+        let { id, varType } = node.data;
+        let idStr = id.lexeme;
+        //Check if already
+        if (idStr in this.scope.symbols) {
+            this.reportError("Este identificador já foi declarado", node);
+        }
+        this.scope.symbols[idStr] = { kind: node.nodeKind, data: node.data };
+    }
+}
+
+export function verifyProgram(
+    program: ast.Node<ast.Program>
+): [boolean, SemError[]] {
+    let analyzer = new Checker();
+    analyzer.checkBlock(program.data.block);
+    return [analyzer.errors.length > 0, analyzer.errors];
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { Parser } from "./parse";
 import { inspect } from "./ast";
+import { verifyProgram } from "./check";
 import { readFileSync } from "fs";
 import { exit } from "process";
 import { Node, Program } from "./ast";
 import * as process from "process";
+
+const showErrors = (hasErrors: boolean, errors: any[]) => {
+    if (hasErrors) {
+        console.error(`Foram encontrado(s) ${errors.length} erros: \n`);
+        errors.forEach((err) => console.error(err.message));
+        exit(1);
+    }
+};
 
 const main = () => {
     const filename = process.argv[2];
@@ -12,13 +21,11 @@ const main = () => {
     let [program, errors] = parser.parse();
     let numErrs = errors.length;
     //Reportar erros sintáticos
-    if (numErrs > 0) {
-        console.error(`Foram encontrado(s) ${numErrs} erros sintáticos: \n`);
-        errors.forEach((err) => console.error(err.message));
-        exit(1);
-    }
-    //
+    showErrors(numErrs > 0, errors);
     inspect(program as Node<Program>);
+    let [hasErrors, errs] = verifyProgram(program as Node<Program>);
+    //Reportar Errors semânticos
+    showErrors(hasErrors, errors);
 };
 
 main();
